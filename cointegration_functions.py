39cc54faae1c8c6775eadb3440cc42d4129ff737
levonhakobyan7 <levonhakobyan7@gmail.com>
2023-07-21 16:25:23 +0400

    The inital code for checking the co-integration

--- cointegration_functions.py
+++ cointegration_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from statsmodels.tsa.stattools import adfuller
 import statsmodels.api as sm
+import numpy as np
 
 def check_for_stationarity(X, cutoff=0.05):
     # H_0 in adfuller is unit root exists (non-stationary)
@@ -68,3 +69,39 @@ def pairs_list(l: list):
         for j in range(i+1,len(l)):
             pairs.append( (l[i],l[j]) )
     return pairs
+
+
+def pair_check_log(y: pd.Series,x: pd.Series):
+    log_y = np.log(y)
+    log_x = np.log(x)
+    log_y_stationarity = check_for_stationarity(log_y)
+    log_x_stationarity = check_for_stationarity(log_x)
+    if log_y_stationarity == False and log_x_stationarity == False:
+        #Regressing log_y on log_x
+        regression_results = regress(Y=log_y, X=log_x)
+        residual = regression_results["residuals"]
+        residual_stationarity = check_for_stationarity(residual)
+        if residual_stationarity == True:
+            print(f"The pair {y.name} and {x.name} are co-integrated")
+            return True
+            # TODO 1: return coefficient of regression and the bias.
+
+
+            # return {"is_pair": True, "coefficients_of_regression": regression_results.params,
+            # "residuals": residual}
+            # Do I need to return anything else?
+
+        else:
+            print(f"The pair {y.name} and {x.name} are not co-integrated and the p-value is {adfuller(residual)[1]}")
+            # maybe it is a good idea to return the p-value
+            return False
+
+
+    else:
+        if log_y_stationarity == True and log_x_stationarity == True:
+            print("the log close prices for both stocks are stationary")
+        elif log_y_stationarity == True:
+            print(f"the log close price for {y.name} stock is stationary")
+        elif log_x_stationarity == True:
+            print(f"the log close price for {x.name} stock is stationary")
+        return False
--- pairs_trade.py
+++ pairs_trade.py
@@ -1,14 +1,12 @@
 #Imports
 from load_pkl import get_data
 import numpy as np
-import statsmodels
-import statsmodels.api as sm
-from statsmodels.tsa.stattools import coint, adfuller
 import pandas as pd
 import matplotlib.pyplot as plt
-from cointegration_functions import pair_check, pairs_list
+from cointegration_functions import pair_check, pairs_list, pair_check_log, check_for_stationarity
 
-DATA_TRESHOLD = 7000
+DATA_THRESHOLD = 7000
+
 #TODOS
 
 #TODO 1: Clean the data
@@ -18,7 +16,7 @@ DATA_TRESHOLD = 7000
 
 
 # A list of stock symbols that you want, if the symbol is invalid, the system will ignore
-stock_list = ["PEP", "KO", "SPY", "BTC-USD", "IBM", "DJI", "JPM", "GS"]
+stock_list = ["PEP", "KO", "SPY", "BTC-USD", "IBM", "JPM", "GS"]
 stock_pairs = pairs_list(l = stock_list)
 
 # Choice of headers from ['Adj Close', 'Close', 'Open', 'Volume', 'Low', 'High']
@@ -28,10 +26,6 @@ headers = ['Close']
 stock_data = get_data(stock_list, headers)
 stock_data = stock_data["Close"]
 
-# Checking for co-integration for GS and JPM
-
-# GS_data = stock_data["GS"][6000:]
-# JPM_data = stock_data["JPM"][6000:]
 
 # print(pair_check(y=pepsi_data, x= coke_data))
 
@@ -41,9 +35,25 @@ stock_data = stock_data["Close"]
 
 #In order to check all possible pairs from the stock_list execute the following
 
+
 for pair in stock_pairs:
     y_ticker = pair[0]
     x_ticker = pair[1]
-    y_data = stock_data[y_ticker][DATA_TRESHOLD:]
-    x_data = stock_data[x_ticker][DATA_TRESHOLD:]
-    print(pair_check(y=y_data, x=x_data))
+    y_data = stock_data[y_ticker][DATA_THRESHOLD:]
+    x_data = stock_data[x_ticker][DATA_THRESHOLD:]
+    print(pair_check_log(y=y_data, x=x_data))
+
+
+# IBM close price process is stationary when observed from 2010 till 2023.
+
+# print(check_for_stationarity((stock_data["IBM"][5000:])))
+#
+# plt.plot(stock_data["IBM"][5000:])
+# plt.show()
+
+
+# Results from log approach is more intuitive, than the results with cumulative sums.
+
+# for stock in stock_list:
+#     print(stock_data[stock][DATA_THRESHOLD:])
+
